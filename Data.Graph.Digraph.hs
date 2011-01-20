6178f7def888d89b1bb91e854e45e16046f64f55
fmaste <fmaste@users.noreply.github.com>
2011-01-20 19:57:03 -0200

    Comment

--- Data.Graph.Digraph.hs
+++ Data.Graph.Digraph.hs
@@ -16,7 +16,7 @@ import qualified Data.Map as Map
 -- A map of nodes as keys and a list of the direct successors of that node as value.
 type NodeSuccs node = (Map.Map node [node])
 
--- A map of nodes as keys and a list of the direct predecessors of that node as values.
+-- A map of nodes as keys and a list of the direct predecessors of that node as value.
 type NodePreds node = (Map.Map node [node])
 
 data Digraph node edge = Digraph (NodeSuccs node) (NodePreds node) | TmpDigraph edge
