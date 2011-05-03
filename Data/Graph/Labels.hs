ff709674c0269212c325296334deaeb7b4d0d788
fmaste <fmaste@users.noreply.github.com>
2011-05-03 00:22:36 -0300

    Use MultiMap for Labels

--- Data/Graph/Labels.hs
+++ Data/Graph/Labels.hs
@@ -38,8 +38,8 @@ module Data.Graph.Labels (
 -- IMPORTS
 -------------------------------------------------------------------------------
 
-import qualified Data.Map as Map
 import qualified Data.Set as Set
+import qualified Data.MultiMap as MM
 
 -- DATA DEFINITION
 -------------------------------------------------------------------------------
@@ -51,46 +51,46 @@ data Labels element label = Labels (LabelElements element label) (ElementLabels 
 
 -- A label may appear on any element.
 -- A labels contains a Set of elements.
-type LabelElements element label = Map.Map label (Set.Set element)
+type LabelElements element label = MM.MultiMap label element
 
 -- An element may contain any labels.
 -- An element contains a Set of labels.
-type ElementLabels element label = Map.Map element (Set.Set label)
+type ElementLabels element label = MM.MultiMap element label
 
 -- CONSTRUCTION
 -------------------------------------------------------------------------------
 
 -- The empty Labels.
 empty :: (Ord element, Ord label) => Labels element label
-empty = Labels Map.empty Map.empty
+empty = Labels MM.empty MM.empty
 
 -- Adds a label without any elements relationships.
 -- If this label already exists the original Labels is returned.
 addLabel :: (Ord element, Ord label) => label -> Labels element label -> Labels element label
 addLabel label (Labels labelElements elementLabels) = Labels labelElements' elementLabels where
-	labelElements' = Map.insertWith (\new old -> old) label Set.empty labelElements
+	labelElements' = MM.addKey label labelElements
 
 -- Adds an element without any labels relationships.
 -- If this element already exists the original Labels is returned.
 addElement :: (Ord element, Ord label) => element -> Labels element label -> Labels element label
 addElement element (Labels labelElements elementLabels) = Labels labelElements elementLabels' where
-	elementLabels' = Map.insertWith (\new old -> old) element Set.empty elementLabels
+	elementLabels' = MM.addKey element elementLabels
 
 -- Removes a label and all its elements relationships.
 -- If this label does not exists the original Labels is returned.
 removeLabel :: (Ord element, Ord label) => label -> Labels element label -> Labels element label
 removeLabel label adj@(Labels labelElements elementLabels) = Labels labelElements' elementLabels' where
-	labelElements' = Map.delete label labelElements
+	labelElements' = MM.removeKey label labelElements
 	elementLabels' = foldl f elementLabels $ getLabelElements label adj where
-		f elementLabels'' element = Map.adjust (Set.delete label) element elementLabels''
+		f elementLabels'' element = MM.removeValue element label elementLabels''
 
 -- Removes an element and all its labels relationships.
 -- If this element does not exists the original Labels is returned.
 removeElement :: (Ord element, Ord label) => element -> Labels element label -> Labels element label
 removeElement element adj@(Labels labelElements elementLabels) = Labels labelElements' elementLabels' where
 	labelElements' = foldl f labelElements $ getElementLabels element adj where
-		f labelElements'' label = Map.adjust (Set.delete element) label labelElements''
-	elementLabels' = Map.delete element elementLabels
+		f labelElements'' label = MM.removeValue label element labelElements''
+	elementLabels' = MM.removeKey element elementLabels
 
 -- Adds a label to the element.
 -- If the element or label don't exist they are added.
@@ -99,10 +99,8 @@ removeElement element adj@(Labels labelElements elementLabels) = Labels labelEle
 addElementLabel :: (Ord element, Ord label) => element -> label -> Labels element label -> Labels element label
 addElementLabel element label (Labels labelElements elementLabels) =
 	let 
-		labelElements' = f labelElements label element
-		elementLabels' = f elementLabels element label
-		f parentMap k v = Map.insertWith' g k (Set.singleton v) parentMap where
-			g new old = Set.insert v old
+		labelElements' = MM.addValue label element labelElements
+		elementLabels' = MM.addValue element label elementLabels
 	in Labels labelElements' elementLabels'
 
 -- Removes a label from the element.
@@ -112,63 +110,51 @@ addElementLabel element label (Labels labelElements elementLabels) =
 removeElementLabel :: (Ord element, Ord label) => element -> label -> Labels element label -> Labels element label
 removeElementLabel element label (Labels labelElements elementLabels) = 
 	let 
-		labelElements' = f labelElements label element
-		elementLabels' = f elementLabels element label
-		f parentMap k v = Map.adjust g k parentMap where
-			g aSet = Set.delete v aSet
+		labelElements' = MM.removeValue label element labelElements
+		elementLabels' = MM.removeValue element label elementLabels
 	in Labels labelElements' elementLabels'
 
 -- QUERY
 -------------------------------------------------------------------------------
 
 getLabels :: (Ord element, Ord label) => Labels element label -> [label]
-getLabels (Labels labelElements _) = Map.keys labelElements
+getLabels (Labels labelElements _) = MM.getKeys labelElements
 
 getElements :: (Ord element, Ord label) => Labels element label -> [element]
-getElements (Labels _ elementLabels) = Map.keys elementLabels
+getElements (Labels _ elementLabels) = MM.getKeys elementLabels
 
 getLabelsCount :: (Ord element, Ord label) => Labels element label -> Int
-getLabelsCount (Labels labelElements _) = Map.size labelElements
+getLabelsCount (Labels labelElements _) = MM.getKeyCount labelElements
 
 getElementsCount :: (Ord element, Ord label) => Labels element label -> Int
-getElementsCount (Labels _ elementLabels) = Map.size elementLabels
+getElementsCount (Labels _ elementLabels) = MM.getKeyCount elementLabels
 
 getLabelElements :: (Ord element, Ord label) => label -> Labels element label -> [element]
-getLabelElements label l = 
-	Set.elems $ getLabelElementsSet label l
+getLabelElements label (Labels labelElements _) = MM.getValues label labelElements
 
 getElementLabels :: (Ord element, Ord label) => element -> Labels element label -> [label]
-getElementLabels element l = 
-	Set.elems $ getElementLabelsSet element l
+getElementLabels element (Labels _ elementLabels) = MM.getValues element elementLabels
 
 getLabelElementsSet :: (Ord element, Ord label) => label -> Labels element label -> Set.Set element
-getLabelElementsSet label (Labels labelElements _) = 
-	Map.findWithDefault Set.empty label labelElements
+getLabelElementsSet label (Labels labelElements _) = MM.getValuesSet label labelElements
 
 getElementLabelsSet :: (Ord element, Ord label) => element -> Labels element label -> Set.Set label
-getElementLabelsSet element (Labels _ elementLabels) = 
-	Map.findWithDefault Set.empty element elementLabels
+getElementLabelsSet element (Labels _ elementLabels) = MM.getValuesSet element elementLabels
 
 getLabelElementsCount :: (Ord element, Ord label) => label -> Labels element label -> Int
-getLabelElementsCount label l = 
-	Set.size $ getLabelElementsSet label l
+getLabelElementsCount label (Labels labelElements _) = MM.getValueCount label labelElements
 
 getElementLabelsCount :: (Ord element, Ord label) => element -> Labels element label -> Int
-getElementLabelsCount element l = 
-	Set.size $ getElementLabelsSet element l
+getElementLabelsCount element (Labels _ elementLabels) = MM.getValueCount element elementLabels
 
 containsLabel :: (Ord element, Ord label) => label -> Labels element label -> Bool
-containsLabel label (Labels labelElements _) = 
-	Map.member label labelElements
+containsLabel label (Labels labelElements _) = MM.containsKey label labelElements
 
 containsElement :: (Ord element, Ord label) => element -> Labels element label -> Bool
-containsElement element (Labels _ elementLabels) = 
-	Map.member element elementLabels
+containsElement element (Labels _ elementLabels) = MM.containsKey element elementLabels
 
 labelContainsElement :: (Ord element, Ord label) => label -> element -> Labels element label -> Bool
-labelContainsElement label element l = 
-	Set.member element $ getLabelElementsSet label l
+labelContainsElement label element (Labels labelElements _) = MM.containsValue label element labelElements
 
 elementContainsLabel :: (Ord element, Ord label) => element -> label -> Labels element label -> Bool
-elementContainsLabel element label l = 
-	Set.member label $ getElementLabelsSet element l
+elementContainsLabel element label (Labels _ elementLabels) = MM.containsValue element label elementLabels
