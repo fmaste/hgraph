3811932eb7202fc3af1df7b986ca4a6ef97b3deb
fmaste <fmaste@users.noreply.github.com>
2011-05-13 22:38:58 -0300

    Rename comments

--- Data/BinaryRelation/Test.hs
+++ Data/BinaryRelation/Test.hs
@@ -13,9 +13,9 @@ tests  = [
 	("Add to codomain, check codomain", QC.quickCheck BRQC.prop_addToCodomainCheckCodomain),
 	("Add to domain, check domain count", QC.quickCheck BRQC.prop_addToDomainCheckDomainCount),
 	("Add to codomain, check codomain count", QC.quickCheck BRQC.prop_addToCodomainCheckCodomainCount),
-	("Add to domain, check getCodomain", QC.quickCheck BRQC.prop_addToDomainCheckCodomain),
-	("Add to codomain, check getDomain", QC.quickCheck BRQC.prop_addToCodomainCheckDomain),
-	("Add to domain, check getCodomainCount", QC.quickCheck BRQC.prop_addToDomainCheckCodomainCount),
-	("Add to codomain, check getDomainCount", QC.quickCheck BRQC.prop_addToCodomainCheckDomainCount),
+	("Add to domain, check codomain", QC.quickCheck BRQC.prop_addToDomainCheckCodomain),
+	("Add to codomain, check domain", QC.quickCheck BRQC.prop_addToCodomainCheckDomain),
+	("Add to domain, check codomain count", QC.quickCheck BRQC.prop_addToDomainCheckCodomainCount),
+	("Add to codomain, check domain count", QC.quickCheck BRQC.prop_addToCodomainCheckDomainCount),
 	("Add to domain, check relations", QC.quickCheck BRQC.prop_addToDomainCheckRelations),
 	("Add to codomain, check relations", QC.quickCheck BRQC.prop_addToCodomainCheckRelations)]
