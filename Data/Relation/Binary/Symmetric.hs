5ae062f14d9ff9a8bdcb28a4b2f13f374103857e
fmaste <fmaste@users.noreply.github.com>
2011-05-25 00:36:00 -0300

    Move to Relation binary folder

--- Data/Relation/Binary/Symmetric.hs
+++ Data/Relation/Binary/Symmetric.hs
@@ -2,7 +2,7 @@
 
 -- Generic module to manage binary relationships.
 -- TODO: Make it haddock compatibele!
-module Data.Relation.Symmetric (
+module Data.Relation.Binary.Symmetric (
 	-- Atomic constructor functions.
 	BinaryRelation(),
 	empty,
