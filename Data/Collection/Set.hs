88a2af2ba452182b65068a5ff8b6237a2570c599
fmaste <fmaste@users.noreply.github.com>
2011-06-01 22:26:53 -0300

    Set class

---
+++ Data/Collection/Set.hs
@@ -0,0 +1,28 @@
+-- Author: Federico Mastellone (fmaste@gmail.com)
+
+-- Set class.
+-- TODO: Make it haddock compatibele!
+
+-- MODULE
+-------------------------------------------------------------------------------
+
+{-# LANGUAGE TypeFamilies, TypeSynonymInstances #-}
+module Data.Collection.Set (
+	Set(..) ) where
+
+-- IMPORTS
+-------------------------------------------------------------------------------
+
+import qualified Data.Collection as DC
+
+-- CLASS
+-------------------------------------------------------------------------------
+
+class DC.Collection s => Set s where
+	
+	-- Calculates the union of two Sets.
+	getUnion :: s -> s -> s
+
+	-- Calculates the intersection of two Sets.
+	getIntersection :: s -> s -> s
+
