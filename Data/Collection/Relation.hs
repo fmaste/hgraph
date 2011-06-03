a2f1c88164b2a13dde33de314e268aaadfd7dc13
fmaste <fmaste@users.noreply.github.com>
2011-06-03 11:01:39 -0300

    Use only the Set type families

--- Data/Collection/Relation.hs
+++ Data/Collection/Relation.hs
@@ -22,8 +22,6 @@ import qualified Data.Collection.Set as DCS
 
 class (DC.Collection r, DCS.Set (DomainSet r), DCS.Set (CodomainSet r)) => Relation r where
 	-- The Relation type families.
-	type Domain r
-	type Codomain r
 	type DomainSet r
 	type CodomainSet r
 
@@ -32,28 +30,28 @@ class (DC.Collection r, DCS.Set (DomainSet r), DCS.Set (CodomainSet r)) => Relat
 
 	-- Adds an element to the domain of the Relation.
 	-- If this element already exists the original Relation is returned.
-	addDomainElement :: Domain r -> r -> r
+	addDomainElement :: DC.Element (DomainSet r) -> r -> r
 
 	-- Adds an element to the codomain of the Relation.
 	-- If this element already exists the original Relation is returned.
-	addCodomainElement :: Codomain r -> r -> r
+	addCodomainElement :: DC.Element (CodomainSet r) -> r -> r
 
 	-- Removes an element from the domain of the Relation.
 	-- If this element does not exists the original Relation is returned.
-	removeDomainElement ::Domain r -> r -> r
+	removeDomainElement :: DC.Element(DomainSet r) -> r -> r
 
 	-- Removes an element from the codomain of the Relation.
 	-- If this element does not exists the original Relation is returned.
-	removeCodomainElement :: Codomain r -> r -> r
+	removeCodomainElement :: DC.Element (CodomainSet r) -> r -> r
 
 	-- Adds a relation from a domain element to a codomain one.
 	-- If the domain or codomain element is not present they are added.
 	-- If this relation already exists for this elements the original Relation is returned.
-	addRelation :: Domain r -> Codomain r -> r -> r
+	addRelation :: DC.Element (DomainSet r) -> DC.Element (CodomainSet r) -> r -> r
 
 	-- Removes a relation from a domain element to a codomain one.
 	-- If this relation is not present the original Relation is returned.
-	removeRelation :: Domain r -> Codomain r -> r -> r
+	removeRelation :: DC.Element (DomainSet r) -> DC.Element (CodomainSet r) -> r -> r
 
 	-- ATOMIC QUERY FUNCTIONS
 	-----------------------------------------------------------------------
@@ -62,9 +60,9 @@ class (DC.Collection r, DCS.Set (DomainSet r), DCS.Set (CodomainSet r)) => Relat
 
 	getCodomain :: r -> CodomainSet r
 
-	getRelatedTo :: Domain r -> r -> CodomainSet r
+	getRelatedTo :: DC.Element (DomainSet r) -> r -> CodomainSet r
 
-	getRelatedFrom :: Codomain r -> r -> DomainSet r
+	getRelatedFrom :: DC.Element (CodomainSet r) -> r -> DomainSet r
 
 	-- All the relationships. Elements without relationships are not shown.
 	-- This function can be constructed using other funtions, but it is
