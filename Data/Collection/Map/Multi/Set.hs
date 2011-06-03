39287bf99bc4296a766df738079161bb58967c89
fmaste <fmaste@users.noreply.github.com>
2011-06-03 17:32:04 -0300

    MapSet class

---
+++ Data/Collection/Map/Multi/Set.hs
@@ -0,0 +1,24 @@
+-- Author: Federico Mastellone (fmaste@gmail.com)
+
+-- Association of the type (Key, Set Value) class.
+-- TODO: Make it haddock compatibele!
+
+-- MODULE
+-------------------------------------------------------------------------------
+
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+module Data.Collection.Map.Multi.Set (
+	MapSet(..)  ) where
+
+-- IMPORTS
+-------------------------------------------------------------------------------
+
+import qualified Data.Collection.Set as DCS
+import qualified Data.Collection.Map as DCM
+import qualified Data.Collection.Map.Multi as DCMM
+
+-- CLASS
+-------------------------------------------------------------------------------
+
+class (DCMM.MultiMap a, DCS.Set (DCM.Value a)) => MapSet a
+
