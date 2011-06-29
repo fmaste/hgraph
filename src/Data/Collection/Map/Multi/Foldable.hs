511bfe0186be6b7e018732dc86b21205a8fcafec
fmaste <fmaste@users.noreply.github.com>
2011-06-29 10:48:38 -0300

    Rename varibale

--- src/Data/Collection/Map/Multi/Foldable.hs
+++ src/Data/Collection/Map/Multi/Foldable.hs
@@ -22,49 +22,49 @@ import qualified Data.Collection.Map.Multi as DCMM
 -------------------------------------------------------------------------------
 
 -- Foldable class for MultiMap.
-class DCMM.MultiMap m => Foldable m where
+class DCMM.MultiMap mm => Foldable mm where
 
 	-- Right-associative fold of a Map.
-	foldr :: (DC.Element (DCM.Value m) -> a -> a) -> a -> m -> a
+	foldr :: (DC.Element (DCM.Value mm) -> a -> a) -> a -> mm -> a
 
 	-- Left-associative fold of a Map.
-	foldl :: (a -> DC.Element (DCM.Value m) -> a) -> a -> m -> a
+	foldl :: (a -> DC.Element (DCM.Value mm) -> a) -> a -> mm -> a
 
 	-- Fold over the elements of a Map, associating to the right, but strictly.
-	foldr' :: (DC.Element (DCM.Value m) -> a -> a) -> a -> m -> a
+	foldr' :: (DC.Element (DCM.Value mm) -> a -> a) -> a -> mm -> a
 	foldr' = defaultFoldr'
 
 	-- Fold over the elements of a Map, associating to the left, but strictly.
-	foldl' :: (a -> DC.Element (DCM.Value m) -> a) -> a -> m -> a
+	foldl' :: (a -> DC.Element (DCM.Value mm) -> a) -> a -> mm -> a
 	foldl' = defaultFoldl'
 
 	-- Right-associative fold with key of a Map.
-	foldrWithKey :: (DC.Element (DCM.Keys m) -> DC.Element (DCM.Value m) -> a -> a) -> a -> m -> a
+	foldrWithKey :: (DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> a -> a) -> a -> mm -> a
 
 	-- Left-associative fold with key of a Map.
-	foldlWithKey :: (a -> DC.Element (DCM.Keys m) -> DC.Element (DCM.Value m) -> a) -> a -> m -> a
+	foldlWithKey :: (a -> DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> a) -> a -> mm -> a
 
 	-- Fold over the elements of a Map with key, associating to the right, but strictly.
-	foldrWithKey' :: (DC.Element (DCM.Keys m) -> DC.Element (DCM.Value m) -> a -> a) -> a -> m -> a
+	foldrWithKey' :: (DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> a -> a) -> a -> mm -> a
 	foldrWithKey' = defaultFoldrWithKey'
 
 	-- Fold over the elements of a Map with key, associating to the left, but strictly.
-	foldlWithKey' :: (a -> DC.Element (DCM.Keys m) -> DC.Element (DCM.Value m) -> a) -> a -> m -> a
+	foldlWithKey' :: (a -> DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> a) -> a -> mm -> a
 	foldlWithKey' = defaultFoldlWithKey'
 
-defaultFoldr' :: (DCMM.MultiMap m, Foldable m) => (DC.Element (DCM.Value m) -> a -> a) -> a -> m -> a
+defaultFoldr' :: (DCMM.MultiMap mm, Foldable mm) => (DC.Element (DCM.Value mm) -> a -> a) -> a -> mm -> a
 defaultFoldr' f z0 xs = foldl f' id xs z0 where 
 	f' g x z = g $! f x z
 
-defaultFoldl' :: (DCMM.MultiMap m, Foldable m) => (a -> DC.Element (DCM.Value m) -> a) -> a -> m -> a
+defaultFoldl' :: (DCMM.MultiMap mm, Foldable mm) => (a -> DC.Element (DCM.Value mm) -> a) -> a -> mm -> a
 defaultFoldl' f z0 xs = foldr f' id xs z0 where 
 	f' x g z = g $! f z x
 
-defaultFoldrWithKey' :: (DCMM.MultiMap m, Foldable m) => (DC.Element (DCM.Keys m) -> DC.Element (DCM.Value m) -> a -> a) -> a -> m -> a
+defaultFoldrWithKey' :: (DCMM.MultiMap mm, Foldable mm) => (DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> a -> a) -> a -> mm -> a
 defaultFoldrWithKey' f z0 xs = foldlWithKey f' id xs z0 where 
 	f' g k x z = g $! f k x z
 
-defaultFoldlWithKey' :: (DCMM.MultiMap m, Foldable m) => (a -> DC.Element (DCM.Keys m) -> DC.Element (DCM.Value m) -> a) -> a -> m -> a
+defaultFoldlWithKey' :: (DCMM.MultiMap mm, Foldable mm) => (a -> DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> a) -> a -> mm -> a
 defaultFoldlWithKey' f z0 xs = foldrWithKey f' id xs z0 where 
 	f' k x g z = g $! f z k x
 
