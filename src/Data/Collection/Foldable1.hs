4161b1e9685944f7416eeb70b48b394b111ebe46
fmaste <fmaste@users.noreply.github.com>
2011-06-29 10:58:46 -0300

    Error

--- src/Data/Collection/Foldable1.hs
+++ src/Data/Collection/Foldable1.hs
@@ -26,14 +26,14 @@ class DCF.Foldable c => Foldable1 c where
 	-- A variant of foldr that has no base case.
 	-- May only be applied to non-empty Collections.
 	foldr1 :: (DC.Element c -> DC.Element c -> DC.Element c) -> c -> DC.Element c
-	foldr1 f c = fromMaybe (error "foldr1: empty structure") (foldr mf Nothing c) where
+	foldr1 f c = fromMaybe (error "foldr1: empty structure") (DCF.foldr mf Nothing c) where
 		mf x Nothing = Just x
 		mf x (Just y) = Just (f x y)
 
 	-- A variant of foldl that has no base case.
 	-- May only be applied to non-empty Collections.
 	foldl1 :: (DC.Element c -> DC.Element c -> DC.Element c) -> c -> DC.Element c
-	foldl1 f c = fromMaybe (error "foldl1: empty structure") (foldl mf Nothing c) where
+	foldl1 f c = fromMaybe (error "foldl1: empty structure") (DCF.foldl mf Nothing c) where
 		mf Nothing y = Just y
 		mf (Just x) y = Just (f x y)
 
