4919ab15ae16822f8b93b1b769ba8db9749765f8
fmaste <fmaste@users.noreply.github.com>
2011-06-29 10:46:44 -0300

    Rename variable

--- src/Data/Collection/Map/Multi.hs
+++ src/Data/Collection/Map/Multi.hs
@@ -21,43 +21,43 @@ import qualified Data.Collection.Map as DCM
 -- CLASSES
 -------------------------------------------------------------------------------
 
-class (DCM.Map a, DC.Collection (DCM.Value a)) => MultiMap a where
+class (DCM.Map mm, DC.Collection (DCM.Value mm)) => MultiMap mm where
 
 	-- ATOMIC CONSTRUCTION FUNCTIONS
 	-----------------------------------------------------------------------
 
 	-- Adds a key and associate it to an empty collection.
 	-- If the key already exists the original MultiMap is returned.
-	addKey :: DC.Element (DCM.Keys a) -> a -> a
+	addKey :: DC.Element (DCM.Keys mm) -> mm -> mm
 
 	-- Adds a collection value to key.
 	-- If key does not exist it is added.
-	addToKey :: DC.Element (DCM.Keys a) -> DC.Element (DCM.Value a) -> a -> a
+	addToKey :: DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> mm -> mm
 
 	-- Removes the collection value from the key.
 	-- If key does not exist the original MultiMap is returned.
 	-- If the collection value does not exists the original MultiMap is returned.
-	removeFromKey :: DC.Element (DCM.Keys a) -> DC.Element (DCM.Value a) -> a -> a
+	removeFromKey :: DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> mm -> mm
 
 	-- ATOMIC QUERY FUNCTIONS
 	-----------------------------------------------------------------------
 	
 	-- Collection value exists for the key?
-	containedInKey :: DC.Element (DCM.Keys a) -> DC.Element (DCM.Value a) -> a -> Bool
+	containedInKey :: DC.Element (DCM.Keys mm) -> DC.Element (DCM.Value mm) -> mm -> Bool
 
 	-- The number of collection values that the key has.
-	getValuesCount :: DC.Element (DCM.Keys a) -> a -> Integer
+	getValuesCount :: DC.Element (DCM.Keys mm) -> mm -> Integer
 
 -------------------------------------------------------------------------------
 
 -- Performant functions to operate on more than one element.
 -- No default implementations because they must be performant.
 -- Use fold if your MultiMap if not an instance and you need this functions.
-class MultiMap a => Batch a where
+class MultiMap mm => Batch mm where
 
 	-- Remove the value from all the keys.
-	remove :: DC.Element (DCM.Value a) -> a -> a
+	remove :: DC.Element (DCM.Value mm) -> mm -> mm
 
 	-- Remove the value from the provided keys.
-	removeFromKeys :: [DC.Element (DCM.Keys a)] -> DC.Element (DCM.Value a) -> a -> a
+	removeFromKeys :: [DC.Element (DCM.Keys mm)] -> DC.Element (DCM.Value mm) -> mm -> mm
 
